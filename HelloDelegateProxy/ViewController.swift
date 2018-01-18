ca162438a2d7bbbdbf2ac26266899e6540e3b398
Austin Chen <austin@accodeworks.com>
2018-01-18 11:53:21 -0500

    Add: initial checkin for HelloDelegateProxy

---
+++ HelloDelegateProxy/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  HelloDelegateProxy
+//
+//  Created by Austin Chen on 2018-01-13.
+//  Copyright © 2018 Austin Chen. All rights reserved.
+//
+
+import UIKit
+import DelegateProxy
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.delegate = self // methods methods will be called
+        scrollView.delegateProxy.receive(selector: #selector(UIScrollViewDelegate.scrollViewWillBeginDragging(_:))) { args in
+            print("ac")
+        }
+        
+        // scrollView.delegate = self // methods methods will NOT be called
+    }
+}
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        print("scrollViewWillBeginDragging")
+    }
+}
+
+final class ScrollViewDelegateProxy: DelegateProxy, UIScrollViewDelegate, DelegateProxyType {
+    func resetDelegateProxy(owner: UIScrollView) {
+        self.cachedPlainDelegate = owner.delegate
+        owner.delegate = self
+    }
+}
+
+extension UIScrollView {
+    var delegateProxy: ScrollViewDelegateProxy {
+        return .proxy(for: self)
+    }
+}
+
+
+/*
+@objc protocol MyDelegate: class {
+    @objc optional func helloAC()
+}
+
+class MyClass {
+    weak var delegate: MyDelegate?
+    
+    init() {
+        
+    }
+    
+    func someMethod() {
+        print("helloAC")
+    }
+}
+
+
+final class MyDelegateProxy: DelegateProxy, MyDelegate, DelegateProxyType {
+    func resetDelegateProxy(owner: MyClass) {
+        owner.delegate = self
+    }
+    
+    func helloAC() {
+        print("aa")
+    }
+    
+}
+
+extension MyClass {
+    var delegateProxy: MyDelegateProxy {
+        return .proxy(for: self)
+    }
+}
+*/
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'HelloDelegateProxy' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for HelloDelegateProxy
+  pod 'DelegateProxy', :path => 'DelegateProxy'
+end
